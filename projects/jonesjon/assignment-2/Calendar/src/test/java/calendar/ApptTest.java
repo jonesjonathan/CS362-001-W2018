21f6d507d814f06e7e50964ab138144a7bf41fb5
jonesjonathan <jonesjon@oregonstate.edu>
2018-01-29 22:38:44 -0800

    Added faulty code from assignment 1 and test cases for assignment 2.

--- projects/jonesjon/assignment-2/Calendar/src/test/java/calendar/ApptTest.java
+++ projects/jonesjon/assignment-2/Calendar/src/test/java/calendar/ApptTest.java
@@ -49,6 +49,7 @@ public class ApptTest {
  		          "Birthday Party",
  		         "This is my birthday party.");
 		 assertFalse(appt1.getValid());
+		 assertEquals(null, appt1.toString());
 		 Appt appt2 = new Appt(21,
  		          100 ,
  		          15 ,
@@ -57,6 +58,7 @@ public class ApptTest {
  		          "Birthday Party",
  		         "This is my birthday party.");
 		 assertFalse(appt2.getValid());
+		 assertEquals(null, appt2.toString());
 		 Appt appt3 = new Appt(21,
  		          30 ,
  		          40 ,
@@ -65,6 +67,7 @@ public class ApptTest {
  		          "Birthday Party",
  		         "This is my birthday party.");
 		 assertFalse(appt3.getValid());
+		 assertEquals(null, appt3.toString());
 
 	 }
 
@@ -84,6 +87,52 @@ public class ApptTest {
 			assertEquals(5, appt.getStartMonth());
 			assertEquals(5, appt.getStartMinute());
 		}
-//add more unit tests as you needed
+
+	 @Test //Recurrence
+	  public void test04() throws Throwable {
+			int[] myArray = new int[]{1, 2, 4};
+			Appt appt = new Appt(1, 1, 1, 1, 2018, "Test", "This is a test");
+ 		 	appt.setRecurrence(myArray, 1, 1, 1);
+
+			//Assertions
+			assertTrue(appt.isRecurring());
+			for(int i = 0; i < 3; i++)
+			{
+				assertEquals(myArray[i], appt.getRecurDays()[i]);
+			}
+			assertEquals(1, appt.getRecurNumber());
+			assertEquals(1, appt.getRecurBy());
+			assertEquals(1, appt.getRecurIncrement());
+		}
+
+	@Test
+		public void test05() throws Throwable
+		{
+			Appt appt1 = new Appt(1, 1, 1, 1, 2018, "Test", "This is a test");
+			String goodO = "\t1/1/2018 at 1:1am ,Test, This is a test\n";
+			assertEquals(goodO, appt1.toString());
+
+			Appt appt2 = new Appt(0, 1, 1, 1, 2018, "Test", "This is a test");
+			goodO = "\t1/1/2018 at 12:1am ,Test, This is a test\n";
+			assertEquals(goodO, appt2.toString());
+
+			Appt appt3 = new Appt(13, 1, 1, 1, 2018, "Test", "This is a test");
+			goodO = "\t1/1/2018 at 1:1pm ,Test, This is a test\n";
+			assertEquals(goodO, appt3.toString());
+		}
+
+	@Test //Title and description branch tests
+		public void test06() throws Throwable
+		{
+			Appt appt1 = new Appt(1, 1, 1, 1, 2018, null, "This is a test");
+			assertEquals("", appt1.getTitle());
+
+			Appt appt2 = new Appt(1, 1, 1, 1, 2018, "Test", null);
+			assertEquals("", appt2.getDescription());
+
+			Appt appt3 = new Appt(1, 1, 1, 1, 2018, null, null);
+			assertEquals("", appt3.getTitle());
+			assertEquals("", appt3.getDescription());
+		}
 
 }
--- projects/jonesjon/assignment-2/Calendar/src/test/java/calendar/CalDayTest.java
+++ projects/jonesjon/assignment-2/Calendar/src/test/java/calendar/CalDayTest.java
@@ -15,11 +15,64 @@ public class CalDayTest {
 
 	 @Test
 	  public void test01()  throws Throwable  {
-
+			CalDay c = new CalDay();
+			assertFalse(c.isValid());
+			assertEquals(null, c.iterator());
+			assertNotNull(c.toString());
 	 }
+
 	 @Test
 	  public void test02()  throws Throwable  {
-		 
+			GregorianCalendar cal = new GregorianCalendar(2018, 1, 1);
+			CalDay c = new CalDay(cal);
+
+			assertTrue(c.isValid());
+			assertNotNull(c.iterator());
+			assertEquals(2018, c.getYear());
+			assertEquals(1, c.getMonth());
+			assertEquals(1, c.getDay());
+
+			//Is the linked list empty?
+			assertEquals(null, c.getAppts().peekFirst());
+			assertEquals(0, c.getSizeAppts());
+
+			//Print it even though print is broken
+			c.toString();
 	 }
-//add more unit tests as you needed	
+
+	 @Test
+	 	public void test03() throws Throwable
+		{
+			Appt appt1 = new Appt(25, 1, 1, 1, 2018, "Test", "This is a test");
+			Appt appt2 = new Appt(1, 1, 1, 1, 2018, "Test", "This is a test");
+			Appt appt3 = new Appt(2, 1, 1, 1, 2018, "Test", "This is a test");
+			GregorianCalendar cal = new GregorianCalendar(2018, 1, 1);
+			CalDay c1 = new CalDay(cal);
+			CalDay c2 = new CalDay(cal);
+
+			c1.addAppt(appt1);
+			assertEquals(null, c1.getAppts().peekFirst());
+			assertEquals(0, c1.getSizeAppts());
+
+			c1.addAppt(appt2);
+			c1.addAppt(appt3);
+			assertEquals(appt2, c1.getAppts().peekFirst());
+			assertEquals(appt3, c1.getAppts().peekLast());
+			assertEquals(2, c1.getSizeAppts());
+
+			assertTrue(c1.isValid());
+			//We know toString is broken
+			assertNotNull(c1.toString());
+
+			c2.addAppt(appt3);
+			c2.addAppt(appt2);
+			assertEquals(appt3, c2.getAppts().peekLast());
+			assertEquals(appt2, c2.getAppts().peekFirst());
+			assertEquals(2, c2.getSizeAppts());
+
+			//We know toString is broken
+			assertNotNull(c2.toString());
+			assertTrue(c2.isValid());
+		}
+//add more unit tests as you needed
 }
